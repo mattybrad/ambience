da559aa4177b8bef061861e7947a89d34172a890
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-10-18 00:07:17 +0100

    nice little proof of concept

--- ambience.js
+++ ambience.js
@@ -1,10 +1,21 @@
 var synth1 = new Tone.Synth().toMaster();
 var synth2 = new Tone.Synth().toMaster();
+var synths = [];
+for(var i = 0; i < 8; i++) {
+  synths[i] = new Tone.Synth().toMaster();
+}
 Tone.Transport.bpm.value = 120;
 
+var chords = [
+  ["C4","E4","G4"],
+  ["A3","C4","E4"]
+];
+var chordNum = 0;
 var loop = new Tone.Loop(function(time) {
-  if(Math.random()>0.8) synth1.triggerAttackRelease("C4", "8n", time);
-  if(Math.random()>0.8) synth2.triggerAttackRelease("E4", "8n", time);
-}, "16n");
+  for(var i = 0; i < chords[chordNum].length; i++) {
+    synths[i].triggerAttackRelease(chords[chordNum][i], "4n", time);
+  }
+  if(Math.random()>0.9) chordNum = (chordNum + 1)%2;
+}, "2n");
 loop.start("1m");
 Tone.Transport.start();
