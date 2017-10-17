997df05566611f26e6a4f8d45ccec82201c460c2
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-10-17 23:41:00 +0100

    technically generative music but not very pleasant

--- ambience.js
+++ ambience.js
@@ -1,5 +1,10 @@
-//create a synth and connect it to the master output (your speakers)
-var synth = new Tone.Synth().toMaster();
+var synth1 = new Tone.Synth().toMaster();
+var synth2 = new Tone.Synth().toMaster();
+Tone.Transport.bpm.value = 120;
 
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
+var loop = new Tone.Loop(function(time) {
+  if(Math.random()>0.8) synth1.triggerAttackRelease("C4", "8n", time);
+  if(Math.random()>0.8) synth2.triggerAttackRelease("E4", "8n", time);
+}, "16n");
+loop.start("1m");
+Tone.Transport.start();
