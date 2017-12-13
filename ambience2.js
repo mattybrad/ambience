b3adad9ee67d429a77db0ac942cf2e55d7a27b2a
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-12-13 00:40:32 +0000

    it makes noise!

--- ambience2.js
+++ ambience2.js
@@ -7,6 +7,27 @@ function AmbientChannel(channelType, barsToCompose) {
   this.nextComposition = 0; // time (in ticks) when more scheduling needs to happen
   this.noteQueue = [];
   this.noteQueueStep = 0;
+  
+  // initialise synth voices
+  var synth;
+  switch(channelType) {
+	case "config":
+	
+	break;
+
+	case "chords":
+	synth = new Tone.PolySynth(5, Tone.Synth).toMaster();
+	break;
+
+	case "melody":
+	synth = new Tone.Synth().toMaster();
+	break;
+
+	case "percussion":
+
+	break;
+  }
+  
   this.doComposition = function() {
 	this.noteQueue = []; // reset note queue
     switch(channelType) {
@@ -15,12 +36,35 @@ function AmbientChannel(channelType, barsToCompose) {
       break;
       
       case "chords":
-      
+      this.noteQueue.push([0,["C4","E4","G4"]]);
+      this.noteQueue.push([8,["A4","C4","E4"]]);
       break;
       
       case "melody":
-      this.noteQueue.push([0,"C4"]);
-      this.noteQueue.push([1,"E4"]);
+      this.noteQueue.push([0,"C5"]);
+      this.noteQueue.push([1,"E5"]);
+	  this.noteQueue.push([3,"F5"]);
+	  this.noteQueue.push([5,"D5"]);
+      break;
+      
+      case "percussion":
+      
+      break;
+    }
+  }
+  
+  this.playNote = function(note) {
+    switch(channelType) {
+      case "config":
+      
+      break;
+      
+      case "chords":
+      synth.triggerAttackRelease(note[1],'1n');
+      break;
+      
+      case "melody":
+      synth.triggerAttackRelease(note[1],'4n');
       break;
       
       case "percussion":
@@ -48,7 +92,7 @@ var schedulingLoop = new Tone.Loop(function(time) {
 	var n;
 	for(var j = 0; j < c.noteQueue.length; j ++) {
 		n = c.noteQueue[j];
-		if(n[0] == c.noteQueueStep) console.log(n[1]);
+		if(n[0] == c.noteQueueStep) c.playNote(n);
 	}
 	c.noteQueueStep ++;
   }
