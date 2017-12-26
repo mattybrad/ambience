87c741618f971eb014934015a6d973c4c10d6aa9
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-12-26 16:20:09 +0000

    scheduling chords nicely

--- ambience2.js
+++ ambience2.js
@@ -1,22 +1,53 @@
 // globals
 var channels = []; // will hold refs to config channel, chords, melody, and percussion
 
+// synths
+var chordSynth = new Tone.PolySynth(4, Tone.Synth).toMaster();
+
+// other stuff
+var knownChordSequences = [
+  ["C","Am","F","G"],
+  ["Am","Em","F","Em"],
+  ["C","Em","F","Dm"]
+]
+var chordNotes = {
+  "C": ["C4","E4","G4"],
+  "Dm": ["D4","F4","A4"],
+  "Em": ["E4","G4","B4"],
+  "F": ["F4","A4","C4"],
+  "G": ["G4","B4","D4"],
+  "Am": ["A4","C4","E4"]
+}
+
 // channel class
 function AmbientChannel(channelType, barsToCompose) {
   this.barsToCompose = barsToCompose; // number of bars to schedule at once
   this.nextComposition = 0; // time (in ticks) when more scheduling needs to happen
+  this.previousSequence = null;
+  this.currentSequence = null;
   this.doComposition = function() {
+    this.previousSequence = this.currentSequence;
+    
     switch(channelType) {
       case "config":
       
       break;
       
       case "chords":
-      console.log("CHORDS");
+      console.log("COMPOSE CHORDS");
+      // in future, take account of previous chords
+      var thisChordSequence = _.sample(knownChordSequences);
+      for(var i = 0; i < thisChordSequence.length; i++) {
+        chordSynth.triggerAttackRelease(
+          chordNotes[thisChordSequence[i]],
+          "1n",
+          "+"+i.toString()+"*1n"
+        );
+      }
       break;
       
       case "melody":
-      console.log("MELODY");
+      console.log("COMPOSE MELODY");
       break;
       
       case "percussion":
